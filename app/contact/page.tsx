b274eb248aaf74b9681cbd2ab52af428e4438492
adambolt2 <adameltarzi@gmail.com>
2024-07-09 16:16:30 +0100

    New link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,9 +4,9 @@ const Contact = () => {
   return (
     <div className="xl:w-9.5/10 dark:bg-stone-200 bg-zinc-900 dark:text-black text-white animate-fadeIn mx-auto  min-h-screen">
         <h1 className=' mt-10 text-2xl flex justify-center'>Contact me:</h1>
-        <h2 className='text-xl mt-10'>Linkden:</h2>
+        <h2 className='text-xl mt-10'>LinkedIn:</h2>
         <a className=' text-l dark:text-indigo-700 text-blue-600' href="https://www.linkedin.com/in/adam-eltarzi-45313a23b/" target="_blank" rel="noopener noreferrer">
-            Click here to visit my Linkden Page
+            Click here to visit my LinkedIn Page
         </a>
        
         <h2 className='text-xl mt-20'>Email:</h2>
